e61d2821a976de90b82445ea88f4e5c7a1fbf2f5
varunrau <varunrau@gmail.com>
2013-03-08 20:09:06 -0800

    added simple parsing

--- wolfram.py
+++ wolfram.py
@@ -1,4 +1,5 @@
 from bottle import route, run, template, view, post, request
+import re
 
 @route('/')
 @view('main_template')
@@ -7,8 +8,22 @@ def main():
 
 @post('/query')
 @view('query_template')
-def query():
+def q():
     value = request.forms.get('value')
-    return value
+    values, question = evaluate(value)
+    return dict(values=values, question=question)
+
+def evaluate(query):
+    values_regex = re.compile('\w+ is \d+')
+    question_regex = re.compile('\w+\?')
+    values_matches = values_regex.findall(query)
+    values = {}
+    for match in values_matches:
+        vals = match.split()
+        values[vals[0]] = vals[2]
+    values_matches = question_regex.findall(query)
+    match = values_matches[0]
+    question = match[:-1]
+    return (values, question)
 
 run(host='localhost', port=8000, debug=True)
