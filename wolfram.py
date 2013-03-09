e6ef0c2b6791ccb68d809ded43e07d8e0d9b2a2e
Patrick Lu <lightning.lu7@gmail.com>
2013-03-09 01:21:37 -0800

    "no"

--- wolfram.py
+++ wolfram.py
@@ -123,6 +123,42 @@ def images(filename):
             time_nof = quadratic(1/2*acceleration, velocity_i, -distance)
             accel_nof = (2*(distance - velocity_i*time))/(time*time)
 
+        if velocity_f & acceleration & distance:
+            vinitial_f = math.sqrt(velocity_f*velocity_f - 2*acceleration*distance)
+            if vinitial_f != values["velocity_i"]:
+                values["velocity_i"] = vinitial_f
+                change = True
+        if velocity_f & velocity_i & distance:
+            accel_f = (velocity_f*velocity_f - velocity_i*velocity_i)/(2*distance)
+            if accel_f != values["acceleration"]:
+                values["acceleration"] = accel_f
+                change = True
+        if velocity_f & velocity_i & acceleration:
+            distance_f = (velocity_f*velocity_f - velocity_i*velocity_i)/(2*acceleration)
+            if distance_f != values["distance"]:
+                values["distance"] = distance_f
+                change = True
+        if velocity_f & acceleration & time:
+            vinitial_nod = velocity_f - acceleration * time
+            if vinitial_nod != values["velocity_i"]:
+                values[velocity_i] = vinitial_nod
+                change = True
+        if velocity_f & velocity_i & time:
+            acceleration_nod = (velocity_f - velocity_i)/time
+            if acceleration_nod != values["acceleration"]
+                values["acceleration"] = acceleration_nod
+                change = True
+        if velocity_i & velocity_f & acceleration:
+            time_nod = (velocity_f - velocity_i)/acceleration
+            if time_nod != values["time"]:
+                values["time"] = time_nod
+                change = True
+        if distance & time & velocity_f:
+            vinitial_noaccel
+        vinitial_noaccel = 2*distance/time - velocity_f
+        vfinal_noaccel = 2*distance/time - velocity_i
+        time_noaccel = 2*distance/(velocity_i + velocity_f)
+
 
 
 
